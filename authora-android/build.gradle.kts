3f171b45d8ef6ddef910d17cbb1b3f0a4873f18e
hastagaming <rip.hastagaming@gmail.com>
2026-08-04 05:43:37 +0700

    fix: opt-in ExperimentalMaterial3Api for TopAppBar and ListItem usage [release]

--- authora-android/build.gradle.kts
+++ authora-android/build.gradle.kts
@@ -26,6 +26,9 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf(
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
     }
 }
 
