bbeceeaed76d31230a409c8185ba8fdff4355b7f
hastagaming <rip.hastagaming@gmail.com>
2026-08-04 05:08:05 +0700

    fix javac error

--- authora-android/build.gradle.kts
+++ authora-android/build.gradle.kts
@@ -15,6 +15,11 @@ android {
         minSdk = 24
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildFeatures {
         compose = true
     }
